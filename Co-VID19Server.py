a46f5ce05a45a29e2ff9c1ade7f22ff6d8b0835a
hamed.minaei@gmail.com <AidaHamed1368>
2020-03-30 20:17:43 +1300

    Final Data

--- Co-VID19Server.py
+++ Co-VID19Server.py
@@ -13,17 +13,14 @@ from dash.dependencies import Input, Output
 app = dash.Dash(__name__)
 server = app.server
 
-NumberOfInfected_text = '''   
-
-                    28                 '''
-NumberOfHospitalised_text = '''   
-
-                    2                '''
-
 
 def readCSV():
     FilePath = r"InputData.csv"
-    return pd.read_csv(FilePath)
+    return pd.read_csv(FilePath, engine='python')
+
+
+def filterDay(Df, FilterDay):
+    return Df[Df['Day'] <= FilterDay]
 
 
 def filterandGreoup(Df, FilterDay):
@@ -86,6 +83,7 @@ def convrtArray(OldArray):
 
     return f(OldArray)
 
+Summary=pd.read_csv('Summary.csv', engine='python')
 
 Df = readCSV()
 AnalysedDF = filterandGreoup(Df, Df['Day'].max())
@@ -99,6 +97,38 @@ DfAnalysed2['DateColumn'] = pd.to_datetime(DfAnalysed2['DateColumn'], format='%d
 DfAnalysed2 = DfAnalysed2.sort_values(by=['DateColumn'], ascending=False)
 indexAll = DfAnalysed2.index.tolist()
 
+
+# Sort value for DHB
+Df=filterDay(Df,Df['Day'].max())
+DfAnalysed3 = Df.groupby('DHB').agg({'DHB2015_Name': 'count'})
+DfAnalysed3['DHB'] = DfAnalysed3.index
+DfAnalysed3 = DfAnalysed3.sort_values(by=['DHB2015_Name'], ascending=True)
+xDHB = DfAnalysed3['DHB2015_Name'].tolist()
+yDHB = DfAnalysed3['DHB'].tolist()
+
+traceDHB = go.Bar(x=xDHB, y=yDHB,
+                orientation='h',
+                text=yDHB,
+                textposition='auto',
+                marker=dict(
+                    color='rgba(58,196,127, 0.8)',
+                    line=dict(color='rgba(11, 39, 25, 1.0)', width=1)
+                ))
+
+#Per Age Group
+Df=filterDay(Df,Df['Day'].max())
+DfAnalysed4=Df[['Sex','Age group', 'DHB2015_Name' ]]
+DfAnalysed4 = pd.pivot_table(DfAnalysed4, index='Sex', columns=['Age group'], values='DHB2015_Name', aggfunc='count')
+DfAnalysed4 = DfAnalysed4.fillna(0)
+
+
+TraceMale = go.Bar(x=DfAnalysed4.columns.tolist(), y=DfAnalysed4.loc['Male',:], name='Male' )
+TraceFemale = go.Bar(x=DfAnalysed4.columns.tolist(), y=DfAnalysed4.loc['Female',:],  name='FeMale')
+
+
+print(DfAnalysed4)
+
+
 for index in indexAll:
     DF_Filter = DfAnalysed2[DfAnalysed2['DateColumn'] <= DfAnalysed2.loc[index, 'DateColumn']]
     DfAnalysed2.loc[index, 'DHB2015_Name'] = DF_Filter['DHB2015_Name'].sum()
@@ -108,6 +138,8 @@ x = DfAnalysed2['DHB2015_Name'].tolist()
 
 DfAnalysed2['DateColumn'] = DfAnalysed2['DateColumn'].dt.strftime('%d/%m')
 y = DfAnalysed2['DateColumn'].tolist()
+
+
 
 trace1 = go.Bar(x=x, y=y,
                 orientation='h',
@@ -125,7 +157,7 @@ data = [
         lat=reformatlat(AnalysedDF),
         lon=reformatlon(AnalysedDF),
         mode='markers+text',
-        opacity=0.3,
+        opacity=0.6,
         showlegend=True,
         text=reformatTextnumber(AnalysedDF),
         # hoverinfo='none',
@@ -163,7 +195,8 @@ layout = go.Layout(
             lon=174.77
         ),
         pitch=0,
-        zoom=4.2
+        zoom=4.2,
+
     ),
 )
 
@@ -204,8 +237,8 @@ app.layout = html.Div([
             ),
             html.Div(
                 [
-                    html.H3(children='Number of confirmed cases'),
-                    html.H2(children='514', style={'color': colors['textColor2']})
+                    html.H3(children='Number of confirmed and probable cases'),
+                    html.H2(children=str(Summary.loc[0, 'Con']), style={'color': colors['textColor2']})
                 ],
                 style={'width': '100%', 'display': 'inline-block', 'textAlign': 'center', 'padding': 0,
                        'border-style': 'solid ', 'vertical-align': 'top', 'backgroundColor': colors['Section'],
@@ -214,7 +247,7 @@ app.layout = html.Div([
             html.Div(
                 [
                     html.H3(children='Number of recovered cases '),
-                    html.H2(children='54', style={'color': colors['textColor3']})
+                    html.H2(children=str(Summary.loc[0, 'Reco']), style={'color': colors['textColor3']})
                 ],
                 style={'width': '100%', 'display': 'inline-block', 'textAlign': 'center', 'padding': 0,
                        'border-style': 'solid ', 'vertical-align': 'top', 'backgroundColor': colors['Section'],
@@ -222,7 +255,7 @@ app.layout = html.Div([
             )
             , html.Div(
                 [html.H3(children='Number of cases in hospital'),
-                 html.H2(children='9', style={'color': colors['textColor1']})
+                 html.H2(children=str(Summary.loc[0, 'hos']), style={'color': colors['textColor1']})
                  ],
                 style={'width': '100%', 'display': 'inline-block', 'textAlign': 'center', 'padding': 0,
                        'border-style': ' solid ', 'vertical-align': 'bottom', 'backgroundColor': colors['Section'],
@@ -255,7 +288,7 @@ app.layout = html.Div([
                 id='example-graph',
                 figure={
                     'data': [trace1],
-                    'layout': {'title': {'text': 'Total Number of cases per day', 'font': {"size": 11}},
+                    'layout': {'title': {'text': 'Total Number of cases', 'font': {"size": 11}},
                                'plot_bgcolor': colors['Section'],
                                'paper_bgcolor': colors['Section'],
                                'font': {'color': colors['textColor4'],
@@ -285,15 +318,121 @@ app.layout = html.Div([
                         'vertical-align': 'middle', 'textAlign': 'right', 'backgroundColor': colors['textColor4'],
                         'color': colors['text'], 'textAlign': 'center', 'margin-right': 10, 'margin-left': 10,
                         'margin-top': 10, 'margin-bottom': 10}
-             )
-], style={'backgroundColor': colors['background'], 'color': colors['text']})
+             ),
+    html.Div([
+             html.Div([
+                 dcc.Graph(
+                     id='DHB-graph',
+                     figure={
+                         'data': [traceDHB],
+                         'layout': {'title': {'text': 'Total Number of cases per DHB', 'font': {"size": 11}},
+                                    'plot_bgcolor': colors['Section'],
+                                    'paper_bgcolor': colors['Section'],
+                                    'font': {'color': colors['textColor4'], "size": 8 },
+
+
+                                    }
+                     })
+
+             ], style={'width': '42%', 'display': 'inline-block', 'padding': 10, 'border-style': 'solid ',
+              'vertical-align': 'middle', 'textAlign': 'right', 'backgroundColor': colors['Section'],
+              'color': colors['text'], 'textAlign': 'center', 'margin-right': 10, 'margin-left': 10}
+             ),
+             html.Div([
+                 dcc.Graph(
+                     id='Sex-graph',
+                     figure={
+                         'data': [TraceMale, TraceFemale],
+                         'layout': {'title': {'text': 'Total Number of cases per DHB', 'font': {"size": 11}},
+                                    'plot_bgcolor': colors['Section'],
+                                    'paper_bgcolor': colors['Section'],
+                                    'font': {'color': colors['textColor4'], "size": 8},
+                                    'barmode': 'stack'
+
+                                    }
+                     })
+
+
+             ], style={'width': '42%', 'display': 'inline-block', 'padding': 10, 'border-style': 'solid ',
+              'vertical-align': 'middle', 'textAlign': 'right', 'backgroundColor': colors['Section'],
+              'color': colors['text'], 'textAlign': 'center', 'margin-right': 10, 'margin-left': 10}
+             ),
+
+    ], style={'width': '100%', 'display': 'inline-block', 'padding': 10, 'border-style': 'solid ',
+              'vertical-align': 'middle', 'textAlign': 'right', 'backgroundColor': colors['Section'],
+              'color': colors['text'], 'textAlign': 'center', 'margin-right': 10, 'margin-left': 10}
+    )
+    ], style={'backgroundColor': colors['background'], 'color': colors['text']}
+
+
+
+    )
+
+
+@app.callback(
+    Output('Sex-graph', 'figure'),
+    [Input('Day-slider', 'value')])
+def update_figure(Day):
+    Df = readCSV()
+    Df = filterDay(Df, Day)
+    DfAnalysed4 = Df[['Sex', 'Age group', 'DHB2015_Name']]
+    DfAnalysed4 = pd.pivot_table(DfAnalysed4, index='Sex', columns=['Age group'], values='DHB2015_Name',
+                                 aggfunc='count')
+    DfAnalysed4 = DfAnalysed4.fillna(0)
+
+    TraceMale = go.Bar(x=DfAnalysed4.columns.tolist(), y=DfAnalysed4.loc['Male', :], name='Male')
+    TraceFemale = go.Bar(x=DfAnalysed4.columns.tolist(), y=DfAnalysed4.loc['Female', :], name='FeMale')
+    figure = {
+        'data': [TraceMale, TraceFemale],
+        'layout': {'title': {'text': 'Total Number of cases per DHB', 'font': {"size": 11}},
+                   'plot_bgcolor': colors['Section'],
+                   'paper_bgcolor': colors['Section'],
+                   'font': {'color': colors['textColor4'], "size": 8},
+                   'barmode': 'stack'
+
+                   }
+             }
+    return  figure
+
+
+@app.callback(
+    Output('DHB-graph', 'figure'),
+    [Input('Day-slider', 'value')])
+def update_figure(Day):
+    Df = readCSV()
+    Df = filterDay(Df, Day)
+    DfAnalysed3 = Df.groupby('DHB').agg({'DHB2015_Name': 'count'})
+    DfAnalysed3['DHB'] = DfAnalysed3.index
+    DfAnalysed3 = DfAnalysed3.sort_values(by=['DHB2015_Name'], ascending=True)
+    xDHB = DfAnalysed3['DHB2015_Name'].tolist()
+    yDHB = DfAnalysed3['DHB'].tolist()
+
+    traceDHB = go.Bar(x=xDHB, y=yDHB,
+                    orientation='h',
+                    text=yDHB,
+                    textposition='auto',
+                    marker=dict(
+                        color='rgba(58,196,127, 0.8)',
+                        line=dict(color='rgba(11, 39, 25, 1.0)', width=1)
+                    ))
+
+    figure = {
+        'data': [traceDHB],
+        'layout': {'title': {'text': 'Total Number of cases per DHB', 'font': {"size": 11}},
+                   'plot_bgcolor': colors['Section'],
+                   'paper_bgcolor': colors['Section'],
+                   'font': {'color': colors['textColor4'], "size": 8},
+
+                   }
+    }
+    return figure
+
 
 
 @app.callback(
     Output('Bubble-graph', 'figure'),
     [Input('Day-slider', 'value')])
 def update_figure(Day):
-
     AnalysedDF = filterandGreoup(Df, Day)
     Numbersize = checkNumberSize(AnalysedDF)
     if len(Numbersize) == 1:
